efa46f94cd65d1f938b51bfa6eba99745192d217
Спас <spascho62@gmail.com>
2022-09-15 22:57:56 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Exercise/Conditional_Statements_Exercise/T05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace T05._Godzilla_vs._Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budged = double.Parse(Console.ReadLine());
+            int statisti = int.Parse(Console.ReadLine());
+            double priceClothsPerStatis = double.Parse(Console.ReadLine());
+
+            double priceForCloths = statisti * priceClothsPerStatis;
+            double decoration = budged - (budged * 0.9);
+
+            if (statisti > 150)
+            {
+                priceForCloths -= priceForCloths * 0.1;
+
+
+                if ((decoration + priceForCloths) > budged)
+                {
+                    Console.WriteLine("Not enough money!");
+                    double levaMore = (budged - (decoration + priceForCloths)) * (-1);
+                    Console.WriteLine($"Wingard needs {levaMore:F2} leva more.");
+                }
+                else
+                {
+                    double moneyForFilming = priceForCloths + decoration;
+                    double moneyLeft = budged - moneyForFilming;
+
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {moneyLeft:F2} leva left.");
+                }
+            }
+            else
+            {
+                if ((decoration + priceForCloths) > budged)
+                {
+                    Console.WriteLine("Not enough money!");
+                    double levaMore = (budged - (decoration + priceForCloths)) * (-1);
+                    Console.WriteLine($"Wingard needs {levaMore:F2} leva more.");
+                }
+                else
+                {
+                    double moneyForFilming = priceForCloths + decoration;
+                    double moneyLeft = budged - moneyForFilming;
+
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {moneyLeft:F2} leva left.");
+                }
+            }
+        }
+    }
+}
