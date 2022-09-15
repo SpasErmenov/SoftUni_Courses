262643e41efee03154b6d44220462dbb4908a93c
Спас <spascho62@gmail.com>
2022-09-15 23:00:41 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Exercise/Conditional_Statements_Exercise/T07. Shopping/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace T07._Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double petarBudged = double.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int P = int.Parse(Console.ReadLine());
+
+            double NPrice = N * 250;
+            double MPrice = (0.35 * NPrice) * M;
+            double PPrice = (0.1 * NPrice) * P;
+
+            double finalPrice = NPrice + MPrice + PPrice;
+
+            if (N > M)
+            {
+                finalPrice = finalPrice * 0.85;
+
+                if (petarBudged >= finalPrice)
+                {
+                    double moneyLeft = petarBudged - finalPrice;
+                    Console.WriteLine($"You have {moneyLeft:F2} leva left!");
+                }
+                else
+                {
+                    double moneyNeed = finalPrice - petarBudged;
+                    Console.WriteLine($"Not enough money! You need {moneyNeed:F2} leva more!");
+                }
+            }
+            else
+            {
+                if (petarBudged >= finalPrice)
+                {
+                    double moneyLeft = petarBudged - finalPrice;
+                    Console.WriteLine($"You have {moneyLeft:F2} leva left!");
+                }
+                else
+                {
+                    double moneyNeed = finalPrice - petarBudged;
+                    Console.WriteLine($"Not enough money! You need {moneyNeed:F2} leva more!");
+                }
+            }
+        }
+    }
+}
