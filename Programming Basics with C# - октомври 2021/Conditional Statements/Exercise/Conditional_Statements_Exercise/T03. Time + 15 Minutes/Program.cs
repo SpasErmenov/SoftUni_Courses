e5f325eab53245cbb31dce045d2a751804afea6e
Спас <spascho62@gmail.com>
2022-09-15 22:55:50 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Exercise/Conditional_Statements_Exercise/T03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T03._Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+
+
+            if (hour >= 0 && hour <= 23 && min >= 0 && min <= 59)
+            {
+
+                int minPlusFiveteen = min + 15;
+                int ostMin = minPlusFiveteen % 60;
+
+                if (minPlusFiveteen >= 60)
+                {
+                    hour += 1;
+                }
+                if (hour >= 24)
+                {
+                    hour -= 24;
+                }
+
+                if (ostMin < 10)
+                {
+                    Console.WriteLine($"{hour}:0{ostMin}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hour}:{ostMin}");
+                }
+            }
+        }
+    }
+}
