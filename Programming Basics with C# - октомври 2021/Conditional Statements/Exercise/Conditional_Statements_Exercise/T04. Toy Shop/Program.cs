f25ea3028761683aa71127f2bad28e7188cd6767
Спас <spascho62@gmail.com>
2022-09-15 22:56:57 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Exercise/Conditional_Statements_Exercise/T04. Toy Shop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace T04._Toy_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double vocation = double.Parse(Console.ReadLine());
+            int puzzle = int.Parse(Console.ReadLine());
+            int doll = int.Parse(Console.ReadLine());
+            int bear = int.Parse(Console.ReadLine());
+            int minion = int.Parse(Console.ReadLine());
+            int truck = int.Parse(Console.ReadLine());
+
+            double puzzlePrice = puzzle * 2.6;
+            int dollPrice = doll * 3;
+            double bearPrice = bear * 4.1;
+            double minionPrice = minion * 8.2;
+            int truckPrice = truck * 2;
+
+            int nToys = puzzle + doll + bear + minion + truck;
+            double totalPrice = puzzlePrice + dollPrice + bearPrice + minionPrice + truckPrice;
+            double priceWithDiscount = 0;
+            double rent = 0.1;
+
+            if (nToys >= 50)
+            {
+                priceWithDiscount = totalPrice * 0.25;
+                totalPrice -= priceWithDiscount;
+
+            }
+            totalPrice = totalPrice - totalPrice * 0.1;
+
+            double moneyLeft = totalPrice - vocation;
+            double moneyNotEnough = vocation - totalPrice;
+
+            if (totalPrice >= vocation)
+            {
+                Console.WriteLine($"Yes! {moneyLeft:F2} lv left.");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
