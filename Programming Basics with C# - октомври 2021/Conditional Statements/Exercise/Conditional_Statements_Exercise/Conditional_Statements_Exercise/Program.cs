794b8d6d7c0d57ca2566137962f934de8818682e
Спас <spascho62@gmail.com>
2022-09-15 22:53:39 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Exercise/Conditional_Statements_Exercise/Conditional_Statements_Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T01._Sum_Seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int sec = firstTime + secondTime + thirdTime;
+
+            //int sec = int.Parse(Console.ReadLine());
+
+            int cqlaMin = sec / 60;
+            int ostSec = sec % 60;
+
+            if (sec % 60 == 0)
+            {
+                Console.WriteLine($"{cqlaMin}:00");
+            }
+            else if (ostSec < 10)
+            {
+                Console.WriteLine($"{cqlaMin}:0{ostSec}");
+            }
+            else
+            {
+                Console.WriteLine($"{cqlaMin}:{ostSec}");
+            }
+        }
+    }
+}
