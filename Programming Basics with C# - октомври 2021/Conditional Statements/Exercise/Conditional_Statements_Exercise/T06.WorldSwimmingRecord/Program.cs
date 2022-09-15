6a6c5efe5aa0f921cdff6424990725de7e58a5ee
Спас <spascho62@gmail.com>
2022-09-15 22:59:31 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Exercise/Conditional_Statements_Exercise/T06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace T06.WorldSwimmingRecord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordToBeat = double.Parse(Console.ReadLine());
+            double distanceToSwim = double.Parse(Console.ReadLine());
+            double stoqnVremeZaMetur = double.Parse(Console.ReadLine());
+
+            double putiZabavenoVreme = Math.Floor(distanceToSwim / 15);
+            double zabavenoVreme = (putiZabavenoVreme * 12.5);
+
+            double vremeNaIvan = (distanceToSwim * stoqnVremeZaMetur) + zabavenoVreme;
+
+
+            if (vremeNaIvan < recordToBeat)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {vremeNaIvan:F2} seconds.");
+            }
+            else
+            {
+                double nedostigashtoVreme = vremeNaIvan - recordToBeat;
+                Console.WriteLine($"No, he failed! He was {nedostigashtoVreme:F2} seconds slower.");
+            }
+        }
+    }
+}
