fbdf36d8d41f8e19ab450eab975096e3c5a3f6ff
Спас <spascho62@gmail.com>
2022-09-15 23:01:31 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Exercise/Conditional_Statements_Exercise/T08. Lunch Break/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serialName = Console.ReadLine();
+            double episodeTime = int.Parse(Console.ReadLine());
+            double breakTime = int.Parse(Console.ReadLine());
+
+            double vremeZaHranene = breakTime / 8;
+            double vremeZaOtdih = breakTime / 4;
+
+
+            double izminaloVreme = vremeZaHranene + vremeZaOtdih;
+            double ostavashtoVreme = breakTime - izminaloVreme;
+
+
+            if (ostavashtoVreme >= episodeTime)
+            {
+                ostavashtoVreme = breakTime - izminaloVreme - episodeTime;
+                Console.WriteLine($"You have enough time to watch {serialName} and left with {Math.Ceiling(ostavashtoVreme)} minutes free time.");
+            }
+            else
+            {
+                double needTime = (breakTime - izminaloVreme - episodeTime) * (-1);
+                Console.WriteLine($"You don't have enough time to watch {serialName}, you need {Math.Ceiling(needTime)} more minutes.");
+            }
+        }
+    }
+}
