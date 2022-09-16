52d16329f462d4d001a382dbceee304de7ec5c04
Спас <spascho62@gmail.com>
2022-09-16 18:59:22 +0300

    Adding exercise

---
+++ JS Advanced - септември 2022/Arrays and Nested Arrays/Exercise/T03_AddAndRemoveElements.js
@@ -0,0 +1,24 @@
+function addAndRemoveElements(arrOfCommands){
+    let res = [];
+    let num = 1;
+    for(let command of arrOfCommands){
+        switch(command){
+            case "add":
+                res.push(num);
+                num++;
+                break;
+            case "remove":
+                res.pop();
+                num++;
+                break;
+        }
+    }
+    if(res.length === 0){
+        console.log("Empty")
+    }else{
+        console.log(res.join("\n"));
+    }
+}
+
+
+addAndRemoveElements(['add', 'add', 'add', 'add'])
