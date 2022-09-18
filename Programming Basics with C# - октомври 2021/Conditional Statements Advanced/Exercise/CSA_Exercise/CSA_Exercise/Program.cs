705bbea43278f871f63cdf65b1d21959d9561e05
Спас <spascho62@gmail.com>
2022-09-18 16:43:36 +0300

    Adding exercise.

--- Programming Basics with C# - октомври 2021/Conditional Statements Advanced/Exercise/CSA_Exercise/CSA_Exercise/Program.cs
+++ Programming Basics with C# - октомври 2021/Conditional Statements Advanced/Exercise/CSA_Exercise/CSA_Exercise/Program.cs
@@ -6,7 +6,31 @@ namespace CSA_Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string type = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int s = r * c;
+            double totalPrice = 0;
+
+            switch (type)
+            {
+                case "Premiere":
+                    totalPrice = s * 12;
+                    Console.WriteLine($"{totalPrice:F2} leva");
+                    break;
+                case "Normal":
+                    totalPrice = s * 7.5;
+                    Console.WriteLine($"{totalPrice:F2} leva");
+                    break;
+                case "Discount":
+                    totalPrice = s * 5;
+                    Console.WriteLine($"{totalPrice:F2} leva");
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
---
+++ Programming Basics with C# - октомври 2021/Conditional Statements Advanced/Exercise/CSA_Exercise/T02. Summer Outfit/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace T02._Summer_Outfit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradus = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            string shoes;
+            string outfit;
+
+
+            if (gradus >= 10 && gradus <= 18)
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    case "Afternoon":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (gradus > 18 && gradus <= 24)
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    case "Afternoon":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (gradus >= 25)
+            {
+                switch (time)
+                {
+                    case "Morning":
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    case "Afternoon":
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    case "Evening":
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                        Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {shoes}.");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
