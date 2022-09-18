a1e727c13e726ec3d7a1495f9e263f7c383f33cc
Спас <spascho62@gmail.com>
2022-09-18 16:49:34 +0300

    Adding exercise

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements Advanced/Exercise/CSA_Exercise/T07. Hotel Room/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace T07._Hotel_Room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            double totalPriceApartment = 0;
+            double totalPriceStudio = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+                    studioPrice = 50;
+                    apartmentPrice = 65;
+                    break;
+                case "June":
+                case "September":
+                    studioPrice = 75.20;
+                    apartmentPrice = 68.70;
+                    break;
+                case "July":
+                case "August":
+                    studioPrice = 76;
+                    apartmentPrice = 77;
+                    break;
+                default:
+                    break;
+            }
+            if ((nights > 7 && nights <= 13) && (month == "May" || month == "October"))
+            {
+                studioPrice = studioPrice - (studioPrice * 0.05);
+            }
+            else if (nights > 14 && (month == "May" || month == "October"))
+            {
+                studioPrice = studioPrice - (studioPrice * 0.3);
+            }
+            else if (nights > 14 && (month == "June" || month == "September"))
+            {
+                studioPrice = studioPrice - (studioPrice * 0.2);
+            }
+            if (nights > 14)
+            {
+                apartmentPrice = apartmentPrice - (apartmentPrice * 0.1);
+            }
+            totalPriceApartment = apartmentPrice * nights;
+            totalPriceStudio = studioPrice * nights;
+
+            Console.WriteLine($"Apartment: {totalPriceApartment:F2} lv.");
+            Console.WriteLine($"Studio: {totalPriceStudio:F2} lv.");
+        }
+    }
+}
