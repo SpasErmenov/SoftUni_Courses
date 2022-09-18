300f9c414135f6e644936515ae62e483a8632d6b
Спас <spascho62@gmail.com>
2022-09-18 16:41:59 +0300

    Adding Conditional Statements Advanced - Exercise (CSA) name was too long so i had to make it shorter, so the program can work properly.

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements Advanced/Exercise/CSA_Exercise/CSA_Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSA_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
