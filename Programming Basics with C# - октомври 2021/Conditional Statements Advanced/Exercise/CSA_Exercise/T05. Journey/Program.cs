82a584d52458ba89d56366cefa8d305db2b78e34
Спас <spascho62@gmail.com>
2022-09-18 16:47:00 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements Advanced/Exercise/CSA_Exercise/T05. Journey/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace T05._Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budged = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            string place = "";
+            double totalPrice = 0;
+
+            if (budged <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    totalPrice = budged * 0.3;
+                    place = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    totalPrice = budged * 0.7;
+                    place = "Hotel";
+                }
+
+            }
+            else if (budged <= 1000 && budged > 100)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    totalPrice = budged * 0.4;
+                    place = "Camp";
+                }
+                else if (season == "winter")
+                {
+                    totalPrice = budged * 0.8;
+                    place = "Hotel";
+                }
+            }
+            else if (budged > 1000)
+            {
+                destination = "Europe";
+                place = "Hotel";
+                totalPrice = budged * 0.9;
+
+            }
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{place} - {totalPrice:F2}");
+
+        }
+    }
+}
