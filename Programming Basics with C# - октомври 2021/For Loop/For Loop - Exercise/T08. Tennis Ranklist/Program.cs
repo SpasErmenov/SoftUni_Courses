ffe2ce2fc9ccfc6179c3aa019cef650875f9e77d
Спас <spascho62@gmail.com>
2022-09-19 20:17:12 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/For Loop/For Loop - Exercise/T08. Tennis Ranklist/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace T08._Tennis_Ranklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int turniri = int.Parse(Console.ReadLine());
+            int nachalniTochki = int.Parse(Console.ReadLine());
+
+            string wlr = "as";
+            double points = nachalniTochki;
+            double wins = 0;
+
+
+
+            for (int i = 0; i < turniri; i++)
+            {
+                wlr = Console.ReadLine();
+
+                switch (wlr)
+                {
+                    case "W":
+                        points = points + 2000;
+                        wins = wins + 1;
+                        break;
+                    case "F":
+                        points = points + 1200;
+                        break;
+                    case "SF":
+                        points = points + 720;
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+            double avarage = (points - nachalniTochki) / turniri;
+
+            Console.WriteLine($"Final points: {points}");
+            Console.WriteLine($"Average points: {Math.Floor(avarage)}");
+            Console.WriteLine($"{((wins / turniri) * 100):F2}%");
+        }
+    }
+}
