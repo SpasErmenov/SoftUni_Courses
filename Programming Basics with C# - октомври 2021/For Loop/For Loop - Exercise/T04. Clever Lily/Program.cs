2f31d35a463334f93bbb8fa126f1f57ed7a2508a
Спас <spascho62@gmail.com>
2022-09-19 20:14:19 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/For Loop/For Loop - Exercise/T04. Clever Lily/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace T04._Clever_Lily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int years = int.Parse(Console.ReadLine());
+            double machinePrice = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            int chetni = 0;
+            int neChetni = 0;
+
+            int pari = 0;
+            int x = 0;
+            int brIgrachki = 0;
+            double pariOtIgrachki = 0;
+            double spesteni = 0;
+            double diff = 0;
+
+            for (int i = 1; i <= years; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    chetni = chetni + 1;
+                    pari = pari + 10;
+                    x = x + pari;
+                }
+                else
+                {
+                    neChetni = neChetni + 1;
+                }
+
+            }
+            pariOtIgrachki = neChetni * toyPrice;
+
+            spesteni = (x + pariOtIgrachki) - chetni;
+
+
+            if (spesteni >= machinePrice)
+            {
+                diff = spesteni - machinePrice;
+                Console.WriteLine($"Yes! {diff:F2}");
+            }
+            else
+            {
+                diff = machinePrice - spesteni;
+                Console.WriteLine($"No! {diff:F2}");
+            }
+        }
+    }
+}
