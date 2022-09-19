9e342c45a0c67362d89e8e474094707b45d18a44
Спас <spascho62@gmail.com>
2022-09-19 20:12:49 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/For Loop/For Loop - Exercise/T02. Half Sum Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T02._Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int bigN = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                sum = sum + a;
+                if (a > bigN)
+                {
+                    bigN = a;
+                }
+            }
+            sum = sum - bigN;
+            //Console.WriteLine(bigN);
+            //Console.WriteLine(sum);
+
+            if (bigN == sum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = " + bigN);
+            }
+            else
+            {
+                int diff = Math.Abs(bigN - sum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = " + diff);
+            }
+        }
+    }
+}
