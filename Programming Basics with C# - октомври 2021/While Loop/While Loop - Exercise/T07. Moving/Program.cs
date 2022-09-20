2885334e2ba5c7c3d529baed238ec89e577cdb21
Спас <spascho62@gmail.com>
2022-09-20 23:59:49 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Exercise/T07. Moving/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            int cubicMeters = x * y * z;
+
+            string cmd;
+
+            while ((cmd = Console.ReadLine()) != "Done")
+            {
+                int size = int.Parse(cmd);
+
+                cubicMeters -= size;
+
+                if (cubicMeters < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {cubicMeters * (-1)} Cubic meters more.");
+                    break;
+                }
+            }
+
+            if (cubicMeters > 0)
+            {
+                Console.WriteLine($"{cubicMeters} Cubic meters left.");
+            }
+        }
+    }
+}
