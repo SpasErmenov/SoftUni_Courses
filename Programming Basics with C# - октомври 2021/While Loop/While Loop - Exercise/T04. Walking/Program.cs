4d5d35471234dd8e24f7d8586a8ebe305f05b0b2
Спас <spascho62@gmail.com>
2022-09-20 23:57:53 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Exercise/T04. Walking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace T04._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dailyTarget = 10000;
+            int sum = 0;
+
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                int steps;
+                if (cmd == "Going home")
+                {
+                    steps = int.Parse(Console.ReadLine());
+                    sum += steps;
+                    if (sum < dailyTarget)
+                    {
+                        Console.WriteLine($"{dailyTarget - sum} more steps to reach goal.");
+                        break;
+                    }
+                    else if (sum >= dailyTarget)
+                    {
+                        Console.WriteLine("Goal reached! Good job!");
+                        Console.WriteLine($"{sum - dailyTarget} steps over the goal!");
+                        break;
+                    }
+                }
+                steps = int.Parse(cmd);
+                sum += steps;
+                if (sum >= dailyTarget)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{sum - dailyTarget} steps over the goal!");
+                    break;
+                }
+
+            }
+        }
+    }
+}
