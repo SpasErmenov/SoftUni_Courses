442d5f5dacae2174870a9cbebf12080a69e24e7f
Спас <spascho62@gmail.com>
2022-09-20 23:55:15 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Exercise/While Loop - Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace While_Loop___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookName = Console.ReadLine();
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            bool bookFound = false;
+
+            while (input != "No More Books")
+            {
+                sum += 1;
+
+                if (input == bookName)
+                {
+                    Console.WriteLine($"You checked {sum - 1} books and found it.");
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "No More Books")
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {sum} books.");
+            }
+        }
+    }
+}
