96f1ad2fd06f294bf694403b3fd13ed74dd2a96d
Спас <spascho62@gmail.com>
2022-09-20 23:59:14 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Exercise/T06. Cake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace T06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+
+            string cmd;
+            int cakePieces = lenght * width;
+
+            while ((cmd = Console.ReadLine()) != "STOP")
+            {
+                int cakePiece = int.Parse(cmd);
+                cakePieces -= cakePiece;
+
+                if (cakePieces < 0)
+                {
+                    Console.WriteLine($"No more cake left! You need {cakePieces * (-1)} pieces more.");
+                    break;
+                }
+            }
+
+            if (cakePieces >= 0)
+            {
+                Console.WriteLine($"{cakePieces} pieces are left.");
+            }
+        }
+    }
+}
