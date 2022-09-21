0ff6053769b4c5ed3e785c288a806fc0a8693cbf
Спас <spascho62@gmail.com>
2022-09-21 17:47:17 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/First Steps In Coding/First Steps In Coding - Lab/T09. Yard Greening/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T09._Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kvMetri = double.Parse(Console.ReadLine());
+
+            double cenaNaKvMetar = 7.61;
+
+            double cena = kvMetri * cenaNaKvMetar;
+
+            double discount = cena * 0.18;
+            double finalPrice = cena - discount;
+
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
