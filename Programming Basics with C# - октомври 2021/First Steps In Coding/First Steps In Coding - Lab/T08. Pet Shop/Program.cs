5f9138f5c860371c81d60aadd33de748b3e96906
Спас <spascho62@gmail.com>
2022-09-21 17:46:31 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/First Steps In Coding/First Steps In Coding - Lab/T08. Pet Shop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodDogs = int.Parse(Console.ReadLine());
+            int foodCats = int.Parse(Console.ReadLine());
+
+            double priceD = foodDogs * 2.5;
+            int priceC = foodCats * 4;
+
+            double total = priceD + priceC;
+
+            Console.WriteLine($"{total} lv.");
+        }
+    }
+}
