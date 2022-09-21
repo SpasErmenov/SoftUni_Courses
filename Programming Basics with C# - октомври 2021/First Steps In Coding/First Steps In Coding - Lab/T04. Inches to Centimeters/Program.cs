813fe88d1ff2559d9a5c179e2723aa6e10fc45c8
Спас <spascho62@gmail.com>
2022-09-21 17:41:42 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/First Steps In Coding/First Steps In Coding - Lab/T04. Inches to Centimeters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            const double cm = 2.54;
+
+            double result = inch * cm;
+            Console.WriteLine(result);
+        }
+    }
+}
