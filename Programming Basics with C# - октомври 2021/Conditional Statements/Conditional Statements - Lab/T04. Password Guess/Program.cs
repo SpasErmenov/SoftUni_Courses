07a351f22118b5591740458836d6ad96c83d3e81
Спас <spascho62@gmail.com>
2022-09-22 23:53:39 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/Conditional Statements/Conditional Statements - Lab/T04. Password Guess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T04._Password_Guess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string enterPassword = Console.ReadLine();
+            const string correctPassword = "s3cr3t!P@ssw0rd";
+
+            if (enterPassword == correctPassword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
