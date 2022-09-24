f646b360e1e0d138115893f4f375732ee6b01f3b
Спас <spascho62@gmail.com>
2022-09-24 09:52:41 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/For Loop/For Loop - Lab/T08. Number sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T08._Number_sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int biggestN = int.MinValue;
+            int smallestN = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a > biggestN)
+                {
+                    biggestN = a;
+                }
+                if (a < smallestN)
+                {
+                    smallestN = a;
+                }
+            }
+            Console.WriteLine($"Max number: {biggestN}");
+            Console.WriteLine($"Min number: {smallestN}");
+        }
+    }
+}
