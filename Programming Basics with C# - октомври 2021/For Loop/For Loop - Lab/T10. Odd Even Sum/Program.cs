f19847a96918d687c6cb7bcac2b4f3326c36c268
Спас <spascho62@gmail.com>
2022-09-24 09:54:17 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/For Loop/For Loop - Lab/T10. Odd Even Sum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace T10._Odd_Even_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int chetni = 0;
+            int neChetni = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    chetni = chetni + a;
+                }
+                else
+                {
+                    neChetni = neChetni + a;
+                }
+
+            }
+
+            if (chetni == neChetni)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = " + chetni);
+            }
+            else
+            {
+                int diff = Math.Abs(chetni - neChetni);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = " + diff);
+            }
+        }
+    }
+}
