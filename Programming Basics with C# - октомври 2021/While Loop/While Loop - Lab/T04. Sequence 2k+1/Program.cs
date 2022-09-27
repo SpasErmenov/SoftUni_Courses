ab7be0f3cd03239d78dc28f5e7eccdb5c90510f3
Спас <spascho62@gmail.com>
2022-09-27 15:39:59 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Lab/T04. Sequence 2k+1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T04._Sequence_2k_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int formula = 1;
+
+            while (n >= formula)
+            {
+                Console.WriteLine(formula);
+                formula = formula * 2 + 1;
+            }
+        }
+    }
+}
