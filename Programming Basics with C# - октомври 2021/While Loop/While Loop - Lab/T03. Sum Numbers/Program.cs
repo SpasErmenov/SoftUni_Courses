bf51c3086ace1cfb82ca3dd38791fad8d229c3d1
Спас <spascho62@gmail.com>
2022-09-27 15:39:16 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Lab/T03. Sum Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T03._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value = true;
+
+            int x = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while (value == true)
+            {
+                int y = int.Parse(Console.ReadLine());
+
+                sum = sum + y;
+
+                if (sum >= x)
+                {
+                    Console.WriteLine(sum);
+                    value = false;
+                }
+
+
+            }
+        }
+    }
+}
