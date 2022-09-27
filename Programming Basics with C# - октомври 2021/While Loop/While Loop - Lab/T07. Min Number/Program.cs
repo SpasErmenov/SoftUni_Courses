8a89cc489d6b8c1a589ccb6b01b34c479ab056d7
Спас <spascho62@gmail.com>
2022-09-27 15:42:54 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Lab/T07. Min Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T07._Min_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double smallN = double.MaxValue;
+
+            while (input != "Stop")
+            {
+                double x = double.Parse(input);
+                if (x < smallN)
+                {
+                    smallN = x;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(smallN);
+        }
+    }
+}
