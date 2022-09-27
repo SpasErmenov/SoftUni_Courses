5d4339daa944eb3c9fcee3b5371808bf85df8d11
Спас <spascho62@gmail.com>
2022-09-27 15:40:45 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Lab/T05. Account Balance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T05._Account_Balance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sum = 0;
+
+            while (input != "NoMoreMoney")
+            {
+                double x = double.Parse(input);
+
+                if (x < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                sum += x;
+                Console.WriteLine($"Increase: {x:F2}");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:F2}");
+        }
+    }
+}
