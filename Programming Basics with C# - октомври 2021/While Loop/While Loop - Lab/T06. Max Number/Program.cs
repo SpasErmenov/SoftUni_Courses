cd46bf18584f7ed79561764c3ea9d401ebcdc4c0
Спас <spascho62@gmail.com>
2022-09-27 15:41:25 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/While Loop/While Loop - Lab/T06. Max Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double bigN = double.MinValue;
+
+            while (input != "Stop")
+            {
+                double x = double.Parse(input);
+                if (x > bigN)
+                {
+                    bigN = x;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(bigN);
+        }
+    }
+}
