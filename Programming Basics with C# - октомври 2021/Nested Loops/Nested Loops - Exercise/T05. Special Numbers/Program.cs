0025ee738c51e6d5df836c4f9ae87a912ea82a51
Спас <spascho62@gmail.com>
2022-09-28 17:02:54 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/Nested Loops/Nested Loops - Exercise/T05. Special Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int a = i / 1 % 10;
+                int b = i / 10 % 10;
+                int c = i / 100 % 10;
+                int d = i / 1000 % 10;
+
+                if (a == 0 || b == 0 || c == 0 || d == 0)
+                {
+                    continue;
+                }
+
+
+                if ((N % a == 0) && (N % b == 0) && (N % c == 0) && (N % d == 0))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
