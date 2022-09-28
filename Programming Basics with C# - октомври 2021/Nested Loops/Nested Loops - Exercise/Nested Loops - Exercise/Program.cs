17ecd100293d0209f7574adb5640d36d1213af72
Спас <spascho62@gmail.com>
2022-09-28 16:59:00 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/Nested Loops/Nested Loops - Exercise/Nested Loops - Exercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Nested_Loops___Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            bool isBigger = false;
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"{num} ");
+                    if (num >= n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    num++;
+                }
+                Console.WriteLine();
+                if (isBigger)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
