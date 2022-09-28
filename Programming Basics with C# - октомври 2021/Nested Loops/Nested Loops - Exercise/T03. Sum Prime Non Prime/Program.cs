56df45bf0d77c00703cc32e73b06553515ac26bb
Спас <spascho62@gmail.com>
2022-09-28 17:01:07 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/Nested Loops/Nested Loops - Exercise/T03. Sum Prime Non Prime/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace T03._Sum_Prime_Non_Prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumPrime = 0;
+            int sumNonPrime = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (String.Equals(line, "stop") == true)
+                {
+                    break;
+                }
+
+                int num = int.Parse(line);
+
+                if (num < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    continue;
+                }
+
+                bool isPrime = true;
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime == true)
+                {
+                    sumPrime += num;
+                }
+                if (isPrime == false)
+                {
+                    sumNonPrime += num;
+                }
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {sumPrime}");
+            Console.WriteLine($"Sum of all non prime numbers is: {sumNonPrime}");
+        }
+    }
+}
