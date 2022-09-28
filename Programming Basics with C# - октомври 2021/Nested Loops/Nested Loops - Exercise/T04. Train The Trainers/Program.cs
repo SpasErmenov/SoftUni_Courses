39507f36a2823f67679725754503cd9dc9d5e0ba
Спас <spascho62@gmail.com>
2022-09-28 17:01:52 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/Nested Loops/Nested Loops - Exercise/T04. Train The Trainers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace T04._Train_The_Trainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double totalAverage = 0;
+            double totalNumberOfPresentations = 0;
+            while (true)
+            {
+                string nameOFPresentation = Console.ReadLine();
+
+                if (nameOFPresentation.Equals("Finish"))
+                {
+                    totalAverage /= (totalNumberOfPresentations * n);
+                    Console.WriteLine($"Student's final assessment is {totalAverage:F2}.");
+                    break;
+                }
+
+                double average = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    double mark = double.Parse(Console.ReadLine());
+                    average += mark;
+                    totalAverage += mark;
+                }
+                average /= n;
+
+                Console.WriteLine($"{nameOFPresentation} - {average:F2}.");
+
+                totalNumberOfPresentations++;
+            }
+        }
+    }
+}
