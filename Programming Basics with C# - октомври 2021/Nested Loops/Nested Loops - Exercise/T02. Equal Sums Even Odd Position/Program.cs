3e088c16039e208a136e654b22e6c54c0acdf237
Спас <spascho62@gmail.com>
2022-09-28 17:00:06 +0300

    Adding exercise.

---
+++ Programming Basics with C# - октомври 2021/Nested Loops/Nested Loops - Exercise/T02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace T02._Equal_Sums_Even_Odd_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            for (int x = a; x <= b; x++)
+            {
+                int sum1 = 0;
+                int sum2 = 0;
+                int num = x;
+                int c = 0;
+                while (num > 0)
+                {
+                    if (c % 2 == 0)
+                    {
+                        sum1 += num % 10;
+                    }
+
+
+                    if (c % 2 == 1)
+                    {
+                        sum2 += num % 10;
+                    }
+
+                    num = num / 10;
+
+                    c++;
+                }
+
+                if (sum1 == sum2)
+                {
+                    Console.Write($"{x} ");
+                }
+            }
+        }
+    }
+}
