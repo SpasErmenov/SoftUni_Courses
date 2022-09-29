c76eb4bab5fb6ec61eea63a7afdffc431c1b2b82
Спас <spascho62@gmail.com>
2022-09-29 17:12:59 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Basic Syntax, Conditional Statements and Loops/Basic Syntax - Lab/T12. Even Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T12._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValid = true;
+            while (isValid == true)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+
+                if (number < 0)
+                {
+                    number = number * (-1);
+                }
+
+                if (number % 2 == 1)
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number is: {number}");
+                    isValid = false;
+                }
+            }
+        }
+    }
+}
