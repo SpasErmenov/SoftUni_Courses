fce68e3eb23ccf1a61219a77d8e6a270fdf40a4f
Спас <spascho62@gmail.com>
2022-09-29 17:12:04 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Basic Syntax, Conditional Statements and Loops/Basic Syntax - Lab/T11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (y > 10)
+            {
+                Console.WriteLine($"{x} X {y} = {x * y}");
+            }
+            else
+            {
+
+                for (int i = y; i <= 10; i++)
+                {
+                    Console.WriteLine($"{x} X {i} = {x * i}");
+                }
+            }
+        }
+    }
+}
