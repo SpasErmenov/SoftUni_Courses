3137bf178b10373f97caeb496909ccfa27c7ae24
Спас <spascho62@gmail.com>
2022-09-29 17:10:17 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Basic Syntax, Conditional Statements and Loops/Basic Syntax - Lab/T09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T09._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentOdd = 1 + (i * 2);
+                Console.WriteLine(currentOdd);
+                sum += currentOdd;
+            }
+            Console.WriteLine($"Sum: " + sum);
+        }
+    }
+}
