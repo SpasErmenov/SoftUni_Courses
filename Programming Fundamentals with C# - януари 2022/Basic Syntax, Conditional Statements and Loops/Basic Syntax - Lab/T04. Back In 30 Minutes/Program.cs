0d21246d6b8f8e5bbec7b2789fa3aaa291bea4c6
Спас <spascho62@gmail.com>
2022-09-29 17:05:09 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Basic Syntax, Conditional Statements and Loops/Basic Syntax - Lab/T04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T04._Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                hours++;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+            minutes %= 60;
+
+            if (minutes < 10)
+            {
+                Console.WriteLine($"{hours}:0{minutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hours}:{minutes}");
+            }
+        }
+    }
+}
