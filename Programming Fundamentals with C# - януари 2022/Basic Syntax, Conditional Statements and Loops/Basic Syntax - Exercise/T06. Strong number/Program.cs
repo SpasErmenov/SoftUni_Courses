c3bafc5b661d454f6c8c0b1df5a117f6b24ed293
Спас <spascho62@gmail.com>
2022-10-04 18:32:08 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Basic Syntax, Conditional Statements and Loops/Basic Syntax - Exercise/T06. Strong number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T06._Strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            long factorialSum = 0;
+            for (int i = 0; i <= number.Length - 1; i++)
+            {
+                char currCh = number[i];
+                int currDigit = (int)currCh - 48;
+
+                long currDigFactorial = 1;
+                for (int r = currDigit; r > 1; r--)
+                {
+                    currDigFactorial *= r;
+                }
+
+                factorialSum += currDigFactorial;
+            }
+
+            if (factorialSum == int.Parse(number))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
