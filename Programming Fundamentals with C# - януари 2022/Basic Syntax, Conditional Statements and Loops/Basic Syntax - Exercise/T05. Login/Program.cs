3af74e317e8010ddbd23b97ab34eb56de5f936f8
Спас <spascho62@gmail.com>
2022-10-04 18:31:32 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Basic Syntax, Conditional Statements and Loops/Basic Syntax - Exercise/T05. Login/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace T05._Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //input
+            string username = Console.ReadLine();
+
+            //action
+            //reverse the username
+            string password = string.Empty;
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            //Input password
+            for (int i = 0; i < 4; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (i == 3 && input != password)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                if (input != password)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                else if (input == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+
+
+            }
+
+            //output
+        }
+    }
+}
