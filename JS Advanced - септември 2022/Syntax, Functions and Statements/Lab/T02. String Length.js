1fdde94bf9275bcda86cb1a9140470a2a436b116
Спас <spascho62@gmail.com>
2022-10-05 22:43:51 +0300

    Adding exercise.

---
+++ JS Advanced - септември 2022/Syntax, Functions and Statements/Lab/T02. String Length.js
@@ -0,0 +1,10 @@
+function solve(arg1, arg2, arg3){
+    
+    let sumOfLength = arg1.length + arg2.length + arg3.length
+    let avarageOfLength = Math.floor(sumOfLength / 3)
+
+    console.log(sumOfLength)
+    console.log(avarageOfLength)
+}
+
+solve('chocolate', 'ice cream', 'cake')
