94b839e764f034a44b9369f7afa0e803ddf12a81
Спас <spascho62@gmail.com>
2022-10-07 12:38:28 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Data Types and Variables/DT and Var. - Exercise/T03. Elevator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace T03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            bool done = false;
+
+            int courses = 0;
+            int lastCourse = 0;
+
+            while (done != true)
+            {
+                if (people >= capacity)
+                {
+                    people -= capacity;
+                    courses++;
+                }
+                else
+                {
+                    people = 0;
+                    courses++;
+                    lastCourse++;
+                }
+
+                if (people == 0)
+                {
+                    done = true;
+                }
+            }
+
+            if (courses > 1)
+            {
+                Console.WriteLine(courses);
+            }
+            else
+            {
+                Console.WriteLine(lastCourse);
+            }
+        }
+    }
+}
