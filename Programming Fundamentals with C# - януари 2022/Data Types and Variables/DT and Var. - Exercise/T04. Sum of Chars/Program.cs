d495c82058a83f4e2c65a655e93e0c75025bef8d
Спас <spascho62@gmail.com>
2022-10-07 12:39:04 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Data Types and Variables/DT and Var. - Exercise/T04. Sum of Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T04._Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                char n = char.Parse(Console.ReadLine());
+                sum += (int)n;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
