31de39481ba370ae415d40e9fb57cbdfb803423d
Спас <spascho62@gmail.com>
2022-10-08 18:33:12 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Data Types and Variables/DT and Var. - Exercise/T08. Beer Kegs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace T08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double value1 = 0;
+            double value2 = 0;
+            string name = null;
+
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = float.Parse(Console.ReadLine());
+                double height = float.Parse(Console.ReadLine());
+
+                value1 = Math.PI * (radius * radius) * height;
+                if (value1 > value2)
+                {
+                    value2 = value1;
+                    name = model;
+                }
+
+            }
+            Console.WriteLine(name);
+        }
+    }
+}
