7277fefaea9ee4d001fa8a551fb54fd36e94684c
Спас <spascho62@gmail.com>
2022-10-08 18:31:56 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Data Types and Variables/DT and Var. - Exercise/T07. Water Overflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            const int tankCapacity = 255;
+            int waterInTank = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int litters = int.Parse(Console.ReadLine());
+                waterInTank += litters;
+                if (waterInTank > tankCapacity)
+                {
+                    waterInTank -= litters;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(waterInTank);
+        }
+    }
+}
