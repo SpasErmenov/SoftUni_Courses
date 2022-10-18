26a59645e3871d6f930c457fb927e15cdb10f393
Спас <spascho62@gmail.com>
2022-10-18 18:46:47 +0300

    Adding exercise

---
+++ Programming Fundamentals with C# - януари 2022/Arrays/Arrays - Lab/Arrays - Lab/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Arrays___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine(weekDays[0]);
+                    break;
+                case 2:
+                    Console.WriteLine(weekDays[1]);
+                    break;
+                case 3:
+                    Console.WriteLine(weekDays[2]);
+                    break;
+                case 4:
+                    Console.WriteLine(weekDays[3]);
+                    break;
+                case 5:
+                    Console.WriteLine(weekDays[4]);
+                    break;
+                case 6:
+                    Console.WriteLine(weekDays[5]);
+                    break;
+                case 7:
+                    Console.WriteLine(weekDays[6]);
+                    break;
+                default:
+                    Console.WriteLine("Invalid day!");
+                    break;
+            }
+        }
+    }
+}
