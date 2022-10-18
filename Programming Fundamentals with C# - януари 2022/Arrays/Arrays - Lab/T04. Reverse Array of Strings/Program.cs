18653dba5e180395474535adf5ab05912a68f8dc
Спас <spascho62@gmail.com>
2022-10-18 18:49:06 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Arrays/Arrays - Lab/T04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace T04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letters = Console.ReadLine().Split();
+
+            Array.Reverse(letters);
+            Console.WriteLine(string.Join(" ", letters));
+        }
+    }
+}
