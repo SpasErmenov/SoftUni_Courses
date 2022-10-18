3b14223fecf68a5f1ea3bd0be1ff368645f03f60
Спас <spascho62@gmail.com>
2022-10-18 18:47:45 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Arrays/Arrays - Lab/T02. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int j = n; j > 0; j--)
+            {
+                Console.Write($"{arr[j - 1]} ");
+            }
+        }
+    }
+}
