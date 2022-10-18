37c750fb770a2c82036131a8104e43ed3c1ba772
Спас <spascho62@gmail.com>
2022-10-18 18:50:19 +0300

    Adding exercise.

---
+++ Programming Fundamentals with C# - януари 2022/Arrays/Arrays - Lab/T06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace T06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sumOfEven = 0;
+            int sumOfOdd = 0;
+            int total = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sumOfEven += numbers[i];
+                }
+                else
+                {
+                    sumOfOdd += numbers[i];
+                }
+            }
+            total = sumOfEven - sumOfOdd;
+            Console.WriteLine(total);
+        }
+    }
+}
