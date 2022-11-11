0230af1aae86db3a6700713bdad8ba1092bb1a22
SpasErmenov <spascho62@gmail.com>
2022-11-11 17:49:31 +0200

    Changing folder name

---
+++ JS Advanced - септември 2022/Objects and Composition/Lab/Lab Examples.js
@@ -0,0 +1,12 @@
+function solve(name, population, treasury){
+    let obj = {
+        oName: name,
+        oPop: population,
+        oTrea: treasury
+    }
+    console.log(obj.oName)
+    console.log(obj.oPop)
+    console.log(obj.oTrea)
+}
+
+solve('Tortuga', 7000, 15000)
