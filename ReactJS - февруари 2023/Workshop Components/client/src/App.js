25b3829fc33fd0556bdf3b40c4ecdb528830716b
Спас <spascho62@gmail.com>
2023-03-12 23:11:35 +0200

    Added React workshop: components.

---
+++ ReactJS - февруари 2023/Workshop Components/client/src/App.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+
+import * as userService from './services/userService.js';
+
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Search } from "./components/Search";
+import './App.css';
+import { UserList } from "./components/UserList";
+
+function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        userService.getAll()
+            .then(users => {
+                setUsers(users);
+            })
+            .catch(err => {
+                console.log('Error' + err);
+            });
+    }, []);
+    return (
+        <>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <UserList users={users}/>
+                </section>
+
+            </main>
+
+            <Footer />
+        </>
+
+    );
+}
+
+export default App;
